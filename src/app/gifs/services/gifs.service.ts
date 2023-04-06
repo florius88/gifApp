9fc947c41f8da54cc2d821c2afb7e578a34503f8
florius88 <florsantosbueno@gmail.com>
2023-04-06 18:33:47 +0200

    Usando LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -21,7 +21,26 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+
+    /* Recuperando datos del LocalStorage
+    Esto podria funcionar, pero nos podria devolver un null
+    this._historial = localStorage.getItem('historial');
+
+    Como estoy comprobando antes en el if que el localstorage tenga algo,
+    podemos hacerlo de esa manera y poniendo una admiracion al final para
+    que TypeScript confie en nuestro codigo
+    */
+    /* if (localStorage.getItem('historial')) {
+      this._historial = JSON.parse(localStorage.getItem('historial')!);
+    } */
+
+    /* Otra manera de hacerlo es con los pipes, si es null, que me devuelva un 
+    array vacio     */
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+
+
+  }
 
   buscarGifs(query: string) {
 
@@ -31,6 +50,10 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      // Usando LocalStorage
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+
     }
 
     console.log(this._historial);
